3e24459faf481d79af6131d5ef4180565fa51e3c
mfa4211 <bangfadli4211@gmail.com>
2023-07-12 11:27:28 +0700

    Add project files.

---
+++ Administrasi/Main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Administrasi
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.dataMuridToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGuruToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jadwalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mataPelajaranToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kelasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1459, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dataMuridToolStripMenuItem,
+            this.dataGuruToolStripMenuItem,
+            this.jadwalToolStripMenuItem,
+            this.mataPelajaranToolStripMenuItem,
+            this.kelasToolStripMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(39, 24);
+            this.toolStripSplitButton1.Text = "toolStripSplitButton1";
+            // 
+            // dataMuridToolStripMenuItem
+            // 
+            this.dataMuridToolStripMenuItem.Name = "dataMuridToolStripMenuItem";
+            this.dataMuridToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.dataMuridToolStripMenuItem.Text = "Data Murid";
+            // 
+            // dataGuruToolStripMenuItem
+            // 
+            this.dataGuruToolStripMenuItem.Name = "dataGuruToolStripMenuItem";
+            this.dataGuruToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.dataGuruToolStripMenuItem.Text = "Data Guru";
+            // 
+            // jadwalToolStripMenuItem
+            // 
+            this.jadwalToolStripMenuItem.Name = "jadwalToolStripMenuItem";
+            this.jadwalToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.jadwalToolStripMenuItem.Text = "Jadwal";
+            // 
+            // mataPelajaranToolStripMenuItem
+            // 
+            this.mataPelajaranToolStripMenuItem.Name = "mataPelajaranToolStripMenuItem";
+            this.mataPelajaranToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.mataPelajaranToolStripMenuItem.Text = "Mata Pelajaran";
+            // 
+            // kelasToolStripMenuItem
+            // 
+            this.kelasToolStripMenuItem.Name = "kelasToolStripMenuItem";
+            this.kelasToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.kelasToolStripMenuItem.Text = "Kelas";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1459, 479);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem dataMuridToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataGuruToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jadwalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mataPelajaranToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kelasToolStripMenuItem;
+    }
+}
+
